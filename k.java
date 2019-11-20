cbd66ba579889fb5fc8fe92e140c588596df8d56
Akarshit Srivastava <43616564+akarshit2001@users.noreply.github.com>
2019-11-20 23:18:12 +0530

    Add files via upload

---
+++ k.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Comparator;
+
+
+public class Book {
+    private String BookName;
+    private String Author;
+    private long isbn;
+    private double price;
+
+    Book(String BookName, String Author, long isbn, double price) {
+        this.Author = Author;
+        this.BookName = BookName;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    Book() {
+super();
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return BookName+Author+isbn+price;
+    }
+
+
+
+
+
+
+    
+}
+
